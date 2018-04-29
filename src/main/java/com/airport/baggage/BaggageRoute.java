e18966fbf5a704a4bf23a962cebaa6ddcc75cc70
Mohan Gudey <mgudey3@gatech.edu>
2018-04-29 09:43:35 -0400

    Validation Interface
    Validation Interface to validate sections

--- src/main/java/com/airport/baggage/BaggageRoute.java
+++ src/main/java/com/airport/baggage/BaggageRoute.java
@@ -1,10 +1,20 @@
 package com.airport.baggage;
 
+
+import java.util.Scanner;
+
 public class BaggageRoute {
 
     public static void main(String [] args){
-
         // Read Input
+        Scanner inputString = new Scanner(System.in);
+
+        while(inputString.hasNextLine()){
+
+            System.out.println(inputString.nextLine().trim());
+            
+        }
+
         // Validate Input
         // Process Input
         // Spit the result
